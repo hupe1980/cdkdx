011c753992f5a6851da6a8428e2bbf380e2e71aa
hupe1980 <frankhuebner1980@gmail.com>
2020-05-24 01:09:50 +0200

    Misc

--- packages/cdkdx/src/commands/bundle-command.ts
+++ packages/cdkdx/src/commands/bundle-command.ts
@@ -6,31 +6,40 @@ import Bundler from 'parcel-bundler';
 import { BaseCommand } from './base-command';
 
 export class BundleCommand extends BaseCommand {
+  @Command.Boolean(`--minfy`)
+  public fix: boolean = false;
+
   @Command.Path(`bundle`)
   async execute() {
     const lambdaPath = path.join(this.context.cwd, 'src', 'lambdas');
 
-    const entryFiles = new Array<string>();
+    const entries: Record<string, string> = {};
 
     if (fs.existsSync(lambdaPath)) {
-      fs.readdirSync(lambdaPath).forEach(name => {
-        const entry = path.join(lambdaPath, name, 'index.ts'); 
+      fs.readdirSync(lambdaPath).forEach((name) => {
+        const entry = path.join(lambdaPath, name, 'index.ts');
         if (fs.existsSync(entry)) {
-          entryFiles.push(entry);
+          entries[name] = entry;
         }
       });
     }
 
-    const options: Bundler.ParcelOptions = {
-      outDir: path.join(this.context.cwd, 'lambdas'), // The out directory to put the build files in, defaults to dist
-      outFile: 'index.js', // The name of the outputFile
-      target: 'node',
-      watch: false,
-      detailedReport: false,
-    };
+    await Promise.all(
+      Object.keys(entries).map(async (key) => {
+        const options: Bundler.ParcelOptions = {
+          outDir: path.join(this.context.cwd, 'lambdas', key), // The out directory to put the build files in, defaults to dist
+          outFile: 'index.js', // The name of the outputFile
+          target: 'node',
+          watch: false,
+          detailedReport: false,
+          bundleNodeModules: true,
+          logLevel: 2,
+        };
 
-    const bundler = new Bundler(entryFiles, options);
+        const bundler = new Bundler(entries[key], options);
 
-    await bundler.bundle();
+        return bundler.bundle();
+      })
+    );
   }
 }
