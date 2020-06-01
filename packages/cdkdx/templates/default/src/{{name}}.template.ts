53298f639fcf8f4f390f6abae4f3a570cf3dcff2
hupe1980 <frankhuebner1980@gmail.com>
2020-06-01 23:51:05 +0200

    Misc

--- packages/cdkdx/templates/default/src/{{name}}.template.ts
+++ packages/cdkdx/templates/default/src/{{name}}.template.ts
@@ -3,18 +3,18 @@ import { Construct } from '@aws-cdk/core';
 import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
 
 export interface {{pascalCase name}}Props {
-    readonly environment?: Record<string, string>;
+  readonly environment?: Record<string, string>;
 }
 
 export class {{pascalCase name}} extends Construct {
-    constructor(scope: Construct, id: string, props: {{pascalCase name}}Props = {}) {
-        super(scope, id);
+  constructor(scope: Construct, id: string, props: {{pascalCase name}}Props = {}) {
+    super(scope, id);
 
-        new Function(this, 'DemoFunction', {
-            runtime: Runtime.NODEJS_12_X,
-            handler: 'index.handler',
-            code: Code.fromAsset(path.join(__dirname, 'lambdas', 'demo')),
-            environment: props.environment
-        });
-    }
+    new Function(this, 'DemoFunction', {
+      runtime: Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: Code.fromAsset(path.join(__dirname, 'lambdas', 'demo')),
+      environment: props.environment,
+    });
+  }
 }
