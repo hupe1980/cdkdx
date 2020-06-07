c148616397222c8fc955fd7c52e9ee8040f22348
hupe1980 <frankhuebner1980@gmail.com>
2020-06-07 22:47:53 +0200

    Misc

--- packages/cdkdx/src/commands/bundle-command.ts
+++ packages/cdkdx/src/commands/bundle-command.ts
@@ -14,51 +14,69 @@ export class BundleCommand extends ConstructCommand {
 
   @Command.Path('bundle')
   async execute(): Promise<number> {
-    const lambdaPath = path.join(this.context.cwd, 'src', 'lambdas');
+    const lambdaPath = path.join(
+      this.context.cwd,
+      'src',
+      'lambdas'
+    );
 
     const entries: Record<string, string> = {};
 
     if (fs.existsSync(lambdaPath)) {
-      fs.readdirSync(lambdaPath).forEach((name) => {
-        if (name === SHARED_FOLDER) return;
+      fs.readdirSync(lambdaPath).forEach(
+        (name) => {
+          if (name === SHARED_FOLDER) return;
 
-        const entry = path.join(lambdaPath, name, 'index.ts');
+          const entry = path.join(
+            lambdaPath,
+            name,
+            'index.ts'
+          );
 
-        if (fs.existsSync(entry)) {
-          entries[name] = entry;
+          if (fs.existsSync(entry)) {
+            entries[name] = entry;
+          }
         }
-      });
+      );
     }
 
-    await Promise.all(
-      Object.keys(entries).map(async (key) => {
-        const options: Bundler.ParcelOptions = {
-          outDir: path.join(this.context.cwd, 'lib', 'lambdas', key), // The out directory to put the build files in, defaults to dist
-          outFile: 'index.js', // The name of the outputFile
-          cacheDir: path.join(this.context.cwd, '.cdkdx'),
-          target: 'node',
-          watch: false,
-          detailedReport: false,
-          bundleNodeModules: true,
-          logLevel: 3,
-          minify: this.minify,
-        };
+    // https://github.com/parcel-bundler/parcel/issues/2838
+    for (const key of Object.keys(entries)) {
+      const options: Bundler.ParcelOptions = {
+        outDir: path.join(
+          this.context.cwd,
+          'lib',
+          'lambdas',
+          key
+        ), // The out directory to put the build files in, defaults to dist
+        outFile: 'index.js', // The name of the outputFile
+        cacheDir: path.join(
+          this.context.cwd,
+          '.cdkdx'
+        ),
+        target: 'node',
+        watch: false,
+        detailedReport: false,
+        bundleNodeModules: true,
+        logLevel: 3,
+        minify: this.minify,
+      };
 
-        const bundler = new Bundler(entries[key], options);
+      const bundler = new Bundler(
+        entries[key],
+        options
+      );
 
-        setupExternalsPlugin(bundler);
+      setupExternalsPlugin(bundler);
 
-        // https://github.com/parcel-bundler/parcel/issues/2838
-        return new Promise((resolve) => {
-          bundler.on('bundled', () => {
-            this.context.stdout.write(`\n✅ Lambda ${key} bundled.\n\n`);
-            resolve();
-          });
+      bundler.on('bundled', () => {
+        this.context.stdout.write(
+          `\n✅ Lambda ${key} bundled.\n\n`
+        );
+      });
 
-          bundler.bundle();
-        }); 
-      })
-    );
+      await bundler.bundle();
+    }
 
     return 0;
   }
