3dfb1edc7eebb1630edbd7bbe8b3533d85903b5c
hupe1980 <frankhuebner1980@gmail.com>
2020-07-03 13:32:50 +0200

    feat: Add workspace support

--- packages/cdkdx/src/commands/test-command.ts
+++ packages/cdkdx/src/commands/test-command.ts
@@ -1,17 +1,31 @@
+import glob from 'glob';
 import { Command } from 'clipanion';
+import findWorkspaceRoot from 'find-workspace-root';
 import * as jest from 'jest';
 
 import { ProjectCommand } from './project-command';
 
 export class TestCommand extends ProjectCommand {
-  @Command.Proxy()
-  public jestArgv!: string[];
+  @Command.String('--globPattern')
+  public globPattern: string;
+
+  @Command.Boolean('--watch')
+  @Command.Boolean('-w')
+  public watch = false;
+
+  @Command.Boolean('--updateSnapshot')
+  @Command.Boolean('-u')
+  public updateSnapshot = false;
+
+  @Command.Boolean('--coverage')
+  @Command.Boolean('--collectCoverage')
+  public coverage = false;
 
   @Command.Path('test')
   async execute(): Promise<number> {
     process.env.NODE_ENV = 'test';
 
-    const jestConfig = this.createJestConfig();
+    const jestConfig = await this.createJestConfig();
 
     const argv: string[] = [];
 
@@ -22,22 +36,53 @@ export class TestCommand extends ProjectCommand {
       })
     );
 
-    argv.push(...this.jestArgv);
+    if (this.watch) {
+      argv.push('--watch');
+    }
+    
+    if (this.updateSnapshot) {
+      argv.push('--updateSnapshot');
+    }
+
+    if (this.coverage) {
+      argv.push('--coverage')
+    }
 
     await jest.run(argv);
 
     return 0;
   }
 
-  private createJestConfig() {
+  private async createJestConfig() {
+    const roots = await (async () => {
+      const workspaceRoot = await findWorkspaceRoot();
+
+      if(workspaceRoot) {
+        const pattern = this.globPattern ?? 'packages/*';
+
+        return new Promise((resolve, reject) => {
+          glob(`${workspaceRoot}/${pattern}`, (err, matches) => {
+            if(err) {
+              reject(err);
+            }
+            resolve(matches);
+          })
+        });
+      }
+
+      return [this.context.cwd]
+    })();
+
     const config = {
       transform: {
         '.ts': require.resolve('ts-jest/dist'),
       },
       moduleFileExtensions: ['ts', 'js'],
       collectCoverageFrom: ['src/**/*.ts'],
-      testMatch: ['<rootDir>/**/*.(spec|test).ts'],
-      rootDir: this.context.cwd,
+      //testMatch: ['<rootDir>/**/*.(spec|test).ts'],
+      roots,
+      testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+      //rootDir: this.context.cwd,
       watchPlugins: [
         require.resolve('jest-watch-typeahead/filename'),
         require.resolve('jest-watch-typeahead/testname'),
